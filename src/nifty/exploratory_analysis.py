cf77a0d9972acc647f6a043d4ee0598be00306e2
Abhishek Hosmani <ace.hosmani@gmail.com>
2020-05-18 12:48:33 -0500

    env variables changes

--- src/nifty/exploratory_analysis.py
+++ src/nifty/exploratory_analysis.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import os
 
+from scipy import stats
+
+
 def data_cleaning(df):
     #Checking for null values
     missing_cnt = df.isnull().sum()
@@ -11,20 +14,26 @@ def data_cleaning(df):
         print("No missing values in dateset")
         return
     else:
-        print(missing_cnt)
-
+        for column in df.columns:
+            print("Columns {} has type {}".format(column, df[column].dtypes))
+            # if np.issubdtype(df[column].dtype, np.number):               
+            #     print("{} is a numeric variable".format(column))
+            # else:
+            #     print("{} is a categorical variable".format(column))
 
 def main():
     data_source = './../../data/nifty/'
     data_files = os.listdir(data_source)
+    missing_values = ["n/a", "na", "--"]
     try:
         print("Datasets Found:", data_files)
         for data in data_files:
+
             if not data.endswith(".csv"):
                 continue
-            df = pd.read_csv(data_source+data, header=None)
+            df = pd.read_csv(data_source+data, na_values=missing_values)
             print("DATASET" + data)
-            print(df.head(2))
+            print(df.info())
             data_cleaning(df)
     except Exception as e:
         print("ERROR:   ", e)
